b1f60ea083b17a80d4f153b318c6bba299a89c71
t1n1wall <34793900+andy-wh@users.noreply.github.com>
2015-11-01 21:36:36 +0000

    update mini_httpd to 1.22 to fix CVE-2015-1548 update build script to populate ssl versions in /etc for libcrypto, libssl and racoon

--- branches/freebsd10/build/scripts/6makeimage.sh
+++ branches/freebsd10/build/scripts/6makeimage.sh
@@ -12,6 +12,10 @@ if [ $MW_ARCH = "amd64" ]; then
 	VERSION=$VERSION.$MW_ARCH	
 fi
 
+/usr/bin/strings $MW_BUILDPATH/t1n1fs/usr/lib/libcrypto.so | grep "^OpenSSL " | grep -i freebsd > $MW_BUILDPATH/t1n1fs/etc/libcrypto.version
+/usr/bin/strings $MW_BUILDPATH/t1n1fs/usr/lib/libssl.so | grep "^OpenSSL " > $MW_BUILDPATH/t1n1fs/etc/libssl.version
+$MW_BUILDPATH/t1n1fs/usr/local/sbin/racoon -V | grep OpenSSL > $MW_BUILDPATH/t1n1fs/etc/racoon.version
+
 makemfsroot() {
 	PLATFORM=$1
 	SPARESPACE=$2
