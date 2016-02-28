ab26650941b05aec0f7b6205350813ac32431689
t1n1wall <34793900+andy-wh@users.noreply.github.com>
2016-02-28 22:01:49 +0000

    Add ifAlias support, add function convert_real_interface_to_friendly_interface_name()

--- branches/freebsd10/build/scripts/2makebinaries.sh
+++ branches/freebsd10/build/scripts/2makebinaries.sh
@@ -190,7 +190,7 @@ export CC=gcc46
         cd net-snmp-5.7.3
         ./configure --with-sys-contact="contact" --with-sys-location="location" --without-openssl --with-default-snmp-version="2" \
         --enable-ipv6 --disable-set-support --disable-des --disable-privacy --disable-md5 --disable-debugging --enable-mini-agent --disable-testing-code \
-        --disable-shared-version --disable-shared '--with-out-transports=TCP Unix TCPIPv6 Callback'  '--with-mib-modules=if-mib host mibII/var_route ucd_snmp' \
+        --disable-shared-version --disable-shared '--with-out-transports=TCP Unix TCPIPv6 Callback'  '--with-mib-modules=if-mib host mibII/var_route ucd_snmp utilities/override' \
         --enable-mfd-rewrites --with-defaults
 	make
         install -s agent/snmpd $MW_BUILDPATH/t1n1fs/usr/local/sbin
